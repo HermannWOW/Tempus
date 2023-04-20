7d8bf86d455a66eddc3466d40010dfd412265893
Victor Gerhard Hermann <49209461+HermannWOW@users.noreply.github.com>
2023-04-20 12:47:38 -0300

    Checkin
    Adicionado o CRUD

---
+++ Tempus/Models/ClienteModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Tempus.Models
+{
+    public class ClienteModel
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Digite o nome do cliente")]
+        public string Nome { get; set; }
+        [Required(ErrorMessage = "Digite o CPF do cliente")]
+        public string Cpf { get; set; }
+        [Required(ErrorMessage = "Digite a data de nascimento do cliente")]
+        public DateTime DataNascimento { get; set; }
+        public DateTime DataCadastro { get; set; }
+        public Decimal RendaFamiliar { get; set; }
+    }
+}
---
+++ Tempus/Repositorio/IClienteRepositorio.cs
@@ -0,0 +1,13 @@
+﻿using Tempus.Models;
+
+namespace Tempus.Repositorio
+{
+    public interface IClienteRepositorio
+    {
+        ClienteModel BuscarIdCliente(int id);
+        List<ClienteModel> BuscarClientes();
+        ClienteModel Adicionar(ClienteModel cliente);
+        ClienteModel Alterar(ClienteModel cliente);
+        bool Apagar(int id);
+    }
+}
